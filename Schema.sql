c46a835180f6691100050bb3f26c43240ce35fad
Hossam Barakat <hossam.eldeen@gmail.com>
2018-07-30 19:49:02 +1000

    Add Schema File

---
+++ Schema.sql
@@ -0,0 +1,10 @@
+﻿IF NOT EXISTS (SELECT * FROM sysobjects WHERE NAME='Task' AND XTYPE='U')
+BEGIN
+	CREATE TABLE Task
+	(
+		[ID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+		[AddedOn] datetime DEFAULT GETDATE(),
+		[IsComplete] bit DEFAULT (0),
+		[Description] nvarchar(100)
+	)
+END
