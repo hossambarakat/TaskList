2fb18c7354f6fd0c7cc491964247f7b11198e30f
Hossam Barakat <hossam.eldeen@gmail.com>
2018-09-05 21:06:59 +1000

    Add Cleanup script

---
+++ TaskList/K8s/Clean.ps1
@@ -0,0 +1,7 @@
+﻿kubectl delete service mssql-service --ignore-not-found=true
+kubectl delete service tasks-app-svc --ignore-not-found=true
+kubectl delete deployment tasks-app-deployment --ignore-not-found=true
+kubectl delete deployment mssql-deployment --ignore-not-found=true
+kubectl delete ReplicaSet tasks-app-rs --ignore-not-found=true
+kubectl delete pod tasks-app --ignore-not-found=true
+kubectl delete pvc,pv --all --ignore-not-found=true
