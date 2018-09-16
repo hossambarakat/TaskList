2130b753546cfbeae06a03cb7e74dfc60cdb1bd5
Hossam Barakat <hossam.eldeen@gmail.com>
2018-09-16 10:58:00 +1000

    clear the screen after deleting objects

--- TaskList/K8s/Clean.ps1
+++ TaskList/K8s/Clean.ps1
@@ -1,4 +1,5 @@
-﻿kubectl delete service mssql-service --ignore-not-found=true
+﻿
+kubectl delete service mssql-service --ignore-not-found=true
 kubectl delete service tasks-app-svc --ignore-not-found=true
 kubectl delete deployment tasks-app-deployment --ignore-not-found=true
 kubectl delete deployment mssql-deployment --ignore-not-found=true
@@ -6,3 +7,4 @@ kubectl delete ReplicaSet tasks-app-rs --ignore-not-found=true
 kubectl delete pod tasks-app --ignore-not-found=true
 kubectl delete pvc,pv --all --ignore-not-found=true
 kubectl delete secret mssql --ignore-not-found=true
+clear
